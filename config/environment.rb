42c261a34774a5d725f6a7fe435bf0866ca2e87f
Stella McEwen <mcewen.stella@gmail.com>
2019-06-23 19:24:28 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,10 @@ require 'yaml/store'
 require 'ostruct'
 require 'date'
 
-
 require 'bundler/setup'
 Bundler.require
-
-
-# put the code to connect to the database here
-
-
-require_relative "../artist.rb"
+ 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
